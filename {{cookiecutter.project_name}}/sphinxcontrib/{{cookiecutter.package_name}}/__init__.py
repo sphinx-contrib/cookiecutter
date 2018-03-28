1ee84d7eaf2ff3db776080010b67ab3bd5ec0750
Stephen Finucane <stephen@that.guru>
2018-03-28 23:18:46 +0100

    Add 'setup' function
    This is necessary for any extension so add it here.
    
    We also need to add 'pbr' and 'sphinx' to requirements as this is needed
    for versioning and type annotations, respectively.
    
    Signed-off-by: Stephen Finucane <stephen@that.guru>

--- {{cookiecutter.project_name}}/sphinxcontrib/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/sphinxcontrib/{{cookiecutter.package_name}}/__init__.py
@@ -10,6 +10,15 @@
 
 import pbr.version
 
+if False:
+    # For type annotations
+    from typing import Any, Dict  # noqa
+    from sphinx.application import Sphinx  # noqa
 
 __version__ = pbr.version.VersionInfo(
     '{{cookiecutter.package_name}}').version_string()
+
+
+def setup(app):
+    # type: (Sphinx) -> Dict[unicode, Any]
+    return {'version': __version__, 'parallel_read_safe': True}
