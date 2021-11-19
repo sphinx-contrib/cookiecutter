c149402e3fa3d85e423b00d9ba013b49441c3585
Juan Luis Cano Rodríguez <hello@juanlu.space>
2021-11-19 21:57:13 +0100

    Refine testing

---
+++ {{cookiecutter.project_name}}/tests/test_simple.py
@@ -0,0 +1,25 @@
+# See https://github.com/sphinx-doc/sphinx/issues/7008#issuecomment-573092764
+import pytest
+
+
+def test(app):
+    # app is a Sphinx application object for default sphinx project (`tests/cases/test-root`)
+    app.build()
+
+
+@pytest.mark.sphinx(buildername="latex")
+def test_latex(app):
+    # latex builder is chosen here
+    app.build()
+
+
+@pytest.mark.sphinx(testroot="case1")
+def test_case1(app):
+    # app is a Sphinx application for case1 sphinx project (`tests/cases/test-case1`)
+    app.build()
+
+
+@pytest.mark.sphinx(confoverrides={"master_doc": "content"})
+def test_confoverrides(app):
+    # a Sphinx application configured with given setting
+    app.build()
