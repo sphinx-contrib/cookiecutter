514419ce6518a212a82cbc2d6a2691004cd0a5b0
Juan Luis Cano Rodríguez <hello@juanlu.space>
2021-11-20 19:06:53 +0100

    Demonstrate "-W --keep-going" in tests

--- {{cookiecutter.project_name}}/tests/test_simple.py
+++ {{cookiecutter.project_name}}/tests/test_simple.py
@@ -5,6 +5,10 @@ import pytest
 def test(app):
     # app is a Sphinx application object for default sphinx project
     # (tests/cases/test-root)
+
+    # See https://github.com/sphinx-doc/sphinx/issues/7008#issuecomment-974680232
+    app.warningiserror = app.keep_going = True
+
     app.build()
 
 
